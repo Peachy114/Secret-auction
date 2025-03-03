75f21a75fcc58c5425d78dee11943ce85dda1ba2
Fatma G. Gabuya <gabuyafatma14@gmail.com>
2025-03-03 10:25:44 +0800

    Update Auction.py

--- Auction.py
+++ Auction.py
@@ -1,8 +1,23 @@
 from art import *
 
+
+def finding_winner(bidding):
+    #Determines the winner
+    user_won = ''
+    high_bid = 0
+
+    max(bidding)
+    for bidder in bidding:
+        amount = int(bidding[bidder])
+        if amount > high_bid:
+            high_bid = amount
+            user_won = bidder
+    print(f"The winner is {user_won} with the winning price of {high_bid}")
+
+
 bidders = {}
-winner = {}
 art = logo
+
 
 print(art)
 auction = True
@@ -11,27 +26,11 @@ while auction:
     user_bid = input("What's your bid?: ")
     bidders[user_name] = user_bid
 
-
-    #Add the bidder
-    for add_bidder in bidders:
-        amount_to_bid = bidders[add_bidder]
-        winner[add_bidder] = amount_to_bid
-
     add_bidder = input("Is there other user wants to bid?: ")
-    if add_bidder == 'yes':
-        auction = True
-    else:
+    if add_bidder == 'no':
         auction = False
-
-#Determines the winner
-max = 0
-user_won = ''
-for user in winner:
-    amount = int(winner[user])
-    if amount > max:
-        max = amount
-        user_won = user
-        print(f"The winner is {user_won} with the winning price of {max}")
-        auction=False
-
+        finding_winner(bidders)
+        print("\n" * 20)
+    else:
+        auction = True
 
